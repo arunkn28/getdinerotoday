9f11f7fecd555c16ba14ebc77358ad00f84cb932
arunkn28 <arunkn28@gmail.com>
2020-04-10 19:36:39 +0530

    Login Post

--- user/views.py
+++ user/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login
+
 from django.views import View
 from django.urls import reverse
 
@@ -9,6 +11,16 @@ class LoginView(View):
     def get(self, request):
         return HttpResponse("Hello")
 
+    def post(self, request):
+        data = request.POST
+        username = data['username']
+        password = data['password']
+        user = authenticate(request, username, password)
+        if user:
+            login(request, user)
+        else:
+            return HttpResponse(status=404, data={'msg': 'Invalid User'})
+
 
 class SignUpView(View):
     def get(self, request):
@@ -17,6 +29,6 @@ class SignUpView(View):
     def post(self, request):
         data = request.POST
         profile = Profile.objects.create_user(data['email'], data['password'], data['first_name'],
-                                    data['last_name'], data['phone_number'])
+                                              data['last_name'], data['phone_number'])
         if profile:
             return HttpResponseRedirect(reverse('login'))
