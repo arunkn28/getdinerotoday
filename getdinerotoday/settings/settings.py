b02230b262c73f00ad56f79b967b0e079a55d4d2
arunkn28 <arunkn28@gmail.com>
2020-04-11 13:22:05 +0530

    Added static files from previous proj

--- getdinerotoday/settings/settings.py
+++ getdinerotoday/settings/settings.py
@@ -47,7 +47,7 @@ ROOT_URLCONF = 'getdinerotoday.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -111,3 +111,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
---
+++ getdinerotoday/static/javascripts/application.js
@@ -0,0 +1,39 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+
+//= require lib/jquery/dist/jquery.min
+//= require jquery_ujs
+//= require lib/bootstrap/js/popper.min
+//= require lib/bootstrap/js/bootstrap.min
+
+//= require lib/jasny-bootstrap/js/jasny-bootstrap.min
+//= require lib/slimscroll/jquery.slimscroll.min
+//= require lib/nano-scroll/jquery.nanoscroller.min
+//= require lib/metisMenu/metisMenu.min
+//= require custom
+
+//= require lib/chartjs/chart.min
+//= require lib/chartjs/chartjs-sass
+
+//= require lib/vectormap/jquery-jvectormap-2.0.2.min
+//= require lib/vectormap/jquery-jvectormap-us-aea-en
+
+//= require lib/chart-c3/d3.min
+//= require lib/chart-c3/c3.min
+
+//= require lib/datatables/jquery.dataTables.min
+//= require lib/datatables/dataTables.responsive.min
+//= require lib/toast/jquery.toast.min
+//= require dashboard
+
+//= require activestorage
