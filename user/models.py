1e3f658ed5615e8a8a2cdf4eabde2454d8cfec27
pankaj jakhar <pankaj.jakhar@vidhema.com>
2020-04-12 01:01:18 +0530

    changes

--- user/models.py
+++ user/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class ProfileUserManager(models.Manager):
     def create_user(self, email, password, first_name, last_name, phone_number):
-        user = User.objects.create(email, username=email, password=password, first_name=first_name,
+        user = User.objects.create_user(email=email, username=email, password=password, first_name=first_name,
                                    last_name=last_name)
         profile = Profile(user=user, phone_number=phone_number)
         profile.save()
@@ -15,3 +15,6 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=15)
     objects = ProfileUserManager()
+
+    def __str__(self):
+        return str(self.user.first_name) + " " + str(self.user.last_name)
