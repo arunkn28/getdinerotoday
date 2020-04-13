14f2adf31708f9256138a7e7cb0da3bfa322167d
arunkn28 <arunkn28@gmail.com>
2020-04-13 18:07:31 +0530

    Procfile and updates in settings file

--- getdinerotoday/settings/development.py
+++ getdinerotoday/settings/development.py
@@ -3,6 +3,10 @@ from getdinerotoday.settings.settings import *
 
 DEBUG = True
 
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+
 SECRET_KEY = '!nz#yq7*eo@3d*1(=z=f0jd-&uq!2j#ivns(shit7*b0d_h%ki'
 
 # SECURITY WARNING: update this when you have the production host
